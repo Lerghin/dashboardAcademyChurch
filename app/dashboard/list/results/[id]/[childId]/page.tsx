1b0073ab4e11e521754b103ce1629be8e8610e5c
pabloelias2000 <equipodeapoyo54@gmail.com>
2024-12-05 16:48:21 -0400

    results

--- app/dashboard/list/results/[id]/[childId]/page.tsx
+++ app/dashboard/list/results/[id]/[childId]/page.tsx
@@ -1,14 +1,16 @@
-'use client';
+'use client'
+
 import { API_URL } from "@/app/lib/config";
 import Table from "@/app/components/Table";
 import { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
 
 type NotaMiembroDTO = {
+  idNota: string;
   idModulo: string;
   cedula: string;
   nota: number;
-  statusAprobacion: string;
+  aprobacionCurso: string;
 };
 
 const columns = [
@@ -18,36 +20,63 @@ const columns = [
 ];
 
 const NotesPage = () => {
-  const { id } = useParams<string>();
-  const { idMod } = useParams<string>();
-  console.log(idMod)
+  const { id, childId } = useParams<{ id: string; childId: string }>();
+
   const [notes, setNotes] = useState<NotaMiembroDTO[]>([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [formData, setFormData] = useState({
-    idModulo: id, // Campo oculto para ID de nota
+    idModulo: childId,
     cedula: "",
     nota: "",
-    statusAprobacion: "",
   });
 
   useEffect(() => {
     const fetchNotes = async () => {
-      const response = await fetch(`${API_URL}nota/getList/${id}`, { cache: "no-store" });
-      if (response.ok) {
-        const data = await response.json();
-        setNotes(data.notaMiembroDTOList);
-       
+      if (!formData.idModulo) return;
+
+      try {
+        const response = await fetch(`${API_URL}nota/getList/${formData.idModulo}`, {
+          cache: "no-store",
+        });
+
+        if (response.ok) {
+          const data = await response.json();
+          setNotes(data);
+        } else {
+          
+          console.error("Error al obtener las notas");
+        }
+      } catch (error) {
+        console.error("Error en la solicitud:", error);
       }
     };
-    if (id) fetchNotes();
-  }, [id]);
+
+    if (childId) {
+      setFormData((prevData) => ({
+        ...prevData,
+        idModulo: childId,
+      }));
+      fetchNotes();
+    }
+  }, [childId, formData.idModulo]);
+
+  const renderRow = (item: NotaMiembroDTO) => (
+    <tr
+      key={item.idNota}
+      className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight"
+    >
+      <td className="p-4">{item.cedula}</td>
+      <td className="p-4">{item.nota}</td>
+      <td className="p-4">{item.aprobacionCurso}</td>
+    </tr>
+  );
 
   const handleOpenModal = () => {
     setFormData({
-      idModulo: id, // Inicializamos el campo oculto como vacío
+      idModulo: childId,
       cedula: "",
       nota: "",
-      statusAprobacion: "",
+     
     });
     setIsModalOpen(true);
   };
@@ -64,8 +93,9 @@ const NotesPage = () => {
   };
 
   const handleSaveNote = async () => {
-    const newNote = { ...formData }; // Incluimos idModulo automáticamente
+    const newNote = { ...formData };
     console.log(newNote);
+  
     const response = await fetch(`${API_URL}nota/createnot`, {
       method: "POST",
       headers: {
@@ -73,16 +103,19 @@ const NotesPage = () => {
       },
       body: JSON.stringify(newNote),
     });
-
+  
     if (response.ok) {
       const createdNote = await response.json();
       setNotes([...notes, createdNote]);
+      alert("Nota creada satisfactoriamente");
       handleCloseModal();
     } else {
-      console.error("Error al guardar la nota");
+      // Intentar obtener el mensaje de error desde el cuerpo de la respuesta
+      const errorData = await response.json();
+      const errorMessage = errorData.message || response.statusText; // Si no hay mensaje, usar statusText como fallback
+      alert(`Error: ${errorMessage}`);
     }
   };
-
   return (
     <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
       <h1 className="text-lg font-semibold">Notas del Módulo</h1>
@@ -92,8 +125,9 @@ const NotesPage = () => {
       >
         Agregar Nota
       </button>
+      
       {notes.length > 0 ? (
-        <Table columns={columns} data={notes} />
+        <Table columns={columns} renderRow={renderRow} data={notes} />
       ) : (
         <p>No hay notas disponibles.</p>
       )}
@@ -102,7 +136,6 @@ const NotesPage = () => {
         <div className="fixed inset-0 bg-gray-800 bg-opacity-50 flex items-center justify-center">
           <div className="bg-white p-6 rounded-md w-96">
             <h2 className="text-lg font-semibold mb-4">Agregar Nota</h2>
-            {/* Campo oculto para idNota */}
             <input
               type="hidden"
               name="idNota"
@@ -129,7 +162,7 @@ const NotesPage = () => {
                 className="w-full border rounded-md p-2"
               />
             </div>
-           
+
             <div className="flex justify-end gap-4">
               <button
                 onClick={handleCloseModal}
