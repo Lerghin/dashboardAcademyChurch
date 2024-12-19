41bff51f84c0c7e0f591bcea147290c02fe89978
pabloelias2000 <equipodeapoyo54@gmail.com>
2024-12-19 16:14:15 -0400

    student

--- app/dashboard/list/students/[id]/page.tsx
+++ app/dashboard/list/students/[id]/page.tsx
@@ -1,5 +1,4 @@
 'use client'
-
 import { API_URL } from '@/app/lib/config';
 import { useParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
@@ -20,7 +19,9 @@ export default function SingleStudentPage() {
     sexo: '',
     status: '',
     fecha_ingreso: '',
-    cursosRealizados: {}
+    cursosRealizados: {}, // cursos previos
+    nuevoCurso: '', // nuevo curso que se agregará
+    nuevoNivel: '' // nivel asociado al nuevo curso
   });
 
   useEffect(() => {
@@ -30,7 +31,10 @@ export default function SingleStudentPage() {
         if (!response.ok) throw new Error('Error en la solicitud');
         const result = await response.json();
         setData(result);
-        setFormData(result); // Pre-cargar datos en el formulario
+        setFormData(prevState => ({
+          ...prevState,
+          ...result
+        })); // Pre-cargar datos en el formulario
       } catch (err) {
         if (err instanceof Error) {
           setError(err.message);
@@ -41,7 +45,7 @@ export default function SingleStudentPage() {
     };
     fetchData();
   }, [id]);
-  
+
   if (error) return <p>Error: {error}</p>;
   if (!data) return <p>Cargando...</p>;
 
@@ -54,9 +58,6 @@ export default function SingleStudentPage() {
       [name]: value
     }));
   };
-
-
-
 
   const handleAddCourse = () => {
     if (formData.nuevoCurso && formData.nuevoNivel) {
@@ -72,8 +73,7 @@ export default function SingleStudentPage() {
       }));
     }
   };
-  
-  
+
   const handleRemoveCourse = (curso: string) => {
     const newCourses = { ...formData.cursosRealizados };
     delete newCourses[curso];
@@ -82,7 +82,7 @@ export default function SingleStudentPage() {
       cursosRealizados: newCourses,
     }));
   };
-  
+
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     try {
@@ -99,8 +99,6 @@ export default function SingleStudentPage() {
       setError(err.message);
     }
   };
-  
-
 
   const { nombre, apellido, cedula, fecha_nacimiento, direccion, ocupacion, telefono, sexo, status, fecha_ingreso, cursosRealizados } = data;
 
@@ -114,7 +112,6 @@ export default function SingleStudentPage() {
           <div className="ml-6">
             <h1 className="text-3xl font-semibold">{nombre} {apellido}</h1>
             <p className="text-sm">Cédula: {cedula}</p>
-            {/* Ícono de lápiz para abrir el modal */}
             <button onClick={handleModalToggle} className="ml-4 text-xl text-white">
               ✏️
             </button>
@@ -146,165 +143,156 @@ export default function SingleStudentPage() {
         </div>
       </div>
 
-      {/* Modal de edición */}
-   
       {isModalOpen && (
-  <div className="fixed inset-0 flex justify-center items-center bg-gray-800 bg-opacity-50 h-screen">
-    <div className="bg-white p-8 rounded-lg w-full max-w-lg max-h-[90vh] overflow-y-auto">
-      <h2 className="text-2xl font-semibold mb-4 text-blue-700">Editar Información</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="grid grid-cols-1 gap-4">
-          {/* Los campos existentes */}
-          <input
-            type="text"
-            name="nombre"
-            value={formData.nombre}
-            onChange={handleInputChange}
-            className="p-2 border rounded-md"
-            placeholder="Nombre"
-          />
-          <input
-            type="text"
-            name="apellido"
-            value={formData.apellido}
-            onChange={handleInputChange}
-            className="p-2 border rounded-md"
-            placeholder="Apellido"
-          />
-          <input
-            type="date"
-            name="fecha_nacimiento"
-            value={formData.fecha_nacimiento}
-            onChange={handleInputChange}
-            className="p-2 border rounded-md"
-          />
-          <input
-            type="text"
-            name="direccion"
-            value={formData.direccion}
-            onChange={handleInputChange}
-            className="p-2 border rounded-md"
-            placeholder="Dirección"
-          />
-          <input
-            type="text"
-            name="ocupacion"
-            value={formData.ocupacion}
-            onChange={handleInputChange}
-            className="p-2 border rounded-md"
-            placeholder="Ocupación"
-          />
-          <input
-            type="text"
-            name="telefono"
-            value={formData.telefono}
-            onChange={handleInputChange}
-            className="p-2 border rounded-md"
-            placeholder="Teléfono"
-          />
-          <select
-            name="sexo"
-            value={formData.sexo}
-            onChange={handleInputChange}
-            className="p-2 border rounded-md"
-          >
-            <option value="MASCULINO">Masculino</option>
-            <option value="FEMENINO">Femenino</option>
-           
-          </select>
-       
-             <select
-            name="status"
-            value={formData.status}
-            onChange={handleInputChange}
-            className="p-2 border rounded-md"
-          >
-            <option value="REGULAR"> REGULAR</option>
-            <option value="MIEMBRONUEVO">MIEMBRONUEVO</option>
-            <option value="RETIRADO">RETIRADO</option>
-          </select>
-          <input
-            type="date"
-            name="fecha_ingreso"
-            value={formData.fecha_ingreso}
-            onChange={handleInputChange}
-            className="p-2 border rounded-md"
-          />
+        <div className="fixed inset-0 flex justify-center items-center bg-gray-800 bg-opacity-50 h-screen">
+          <div className="bg-white p-8 rounded-lg w-full max-w-lg max-h-[90vh] overflow-y-auto">
+            <h2 className="text-2xl font-semibold mb-4 text-blue-700">Editar Información</h2>
+            <form onSubmit={handleSubmit}>
+              <div className="grid grid-cols-1 gap-4">
+                <input
+                  type="text"
+                  name="nombre"
+                  value={formData.nombre}
+                  onChange={handleInputChange}
+                  className="p-2 border rounded-md"
+                  placeholder="Nombre"
+                />
+                <input
+                  type="text"
+                  name="apellido"
+                  value={formData.apellido}
+                  onChange={handleInputChange}
+                  className="p-2 border rounded-md"
+                  placeholder="Apellido"
+                />
+                <input
+                  type="date"
+                  name="fecha_nacimiento"
+                  value={formData.fecha_nacimiento}
+                  onChange={handleInputChange}
+                  className="p-2 border rounded-md"
+                />
+                <input
+                  type="text"
+                  name="direccion"
+                  value={formData.direccion}
+                  onChange={handleInputChange}
+                  className="p-2 border rounded-md"
+                  placeholder="Dirección"
+                />
+                <input
+                  type="text"
+                  name="ocupacion"
+                  value={formData.ocupacion}
+                  onChange={handleInputChange}
+                  className="p-2 border rounded-md"
+                  placeholder="Ocupación"
+                />
+                <input
+                  type="text"
+                  name="telefono"
+                  value={formData.telefono}
+                  onChange={handleInputChange}
+                  className="p-2 border rounded-md"
+                  placeholder="Teléfono"
+                />
+                <select
+                  name="sexo"
+                  value={formData.sexo}
+                  onChange={handleInputChange}
+                  className="p-2 border rounded-md"
+                >
+                  <option value="MASCULINO">Masculino</option>
+                  <option value="FEMENINO">Femenino</option>
+                </select>
+                <select
+                  name="status"
+                  value={formData.status}
+                  onChange={handleInputChange}
+                  className="p-2 border rounded-md"
+                >
+                  <option value="REGULAR">REGULAR</option>
+                  <option value="MIEMBRONUEVO">MIEMBRONUEVO</option>
+                  <option value="RETIRADO">RETIRADO</option>
+                </select>
+                <input
+                  type="date"
+                  name="fecha_ingreso"
+                  value={formData.fecha_ingreso}
+                  onChange={handleInputChange}
+                  className="p-2 border rounded-md"
+                />
 
-          {/* Sección para cursos */}
-          <h3 className="mt-6 text-xl font-semibold text-blue-700 h-max">Cursos Realizados</h3>
-          {Object.entries(formData.cursosRealizados).map(([curso, nivel]) => (
-            <div key={curso} className="flex justify-between items-center mb-4">
-              <input
-                type="text"
-                name={`curso_${curso}`}
-                value={curso}
-                onChange={handleInputChange}
-                className="p-2 border rounded-md w-3/4"
-                placeholder="Nombre del Curso"
-                disabled
-              />
-              <input
-                type="text"
-                name={`nivel_${curso}`}
-                value={nivel}
-                onChange={handleInputChange}
-                className="p-2 border rounded-md w-1/4"
-                placeholder="Nivel"
-              />
-              <button
-                type="button"
-                onClick={() => handleRemoveCourse(curso)}
-                className="text-red-500"
-              >
-                Eliminar
-              </button>
-            </div>
-          ))}
+                <h3 className="mt-6 text-xl font-semibold text-blue-700 h-max">Cursos Realizados</h3>
+                {Object.entries(formData.cursosRealizados).map(([curso, nivel]) => (
+                  <div key={curso} className="flex justify-between items-center mb-4">
+                    <input
+                      type="text"
+                      name={`curso_${curso}`}
+                      value={curso}
+                      onChange={handleInputChange}
+                      className="p-2 border rounded-md w-3/4"
+                      placeholder="Nombre del Curso"
+                      disabled
+                    />
+                    <input
+                      type="text"
+                      name={`nivel_${curso}`}
+                      value={nivel}
+                      onChange={handleInputChange}
+                      className="p-2 border rounded-md w-1/4"
+                      placeholder="Nivel"
+                    />
+                    <button
+                      type="button"
+                      onClick={() => handleRemoveCourse(curso)}
+                      className="text-red-500"
+                    >
+                      Eliminar
+                    </button>
+                  </div>
+                ))}
 
-          {/* Campo para agregar un nuevo curso */}
-          <div className="flex justify-between items-center mb-4">
-            <input
-              type="text"
-              name="nuevoCurso"
-              value={formData.nuevoCurso || ''}
-              onChange={handleInputChange}
-              className="p-2 border rounded-md w-3/4"
-              placeholder="Nuevo Curso"
-            />
-            <input
-              type="text"
-              name="nuevoNivel"
-              value={formData.nuevoNivel || ''}
-              onChange={handleInputChange}
-              className="p-2 border rounded-md w-1/4"
-              placeholder="Nivel"
-            />
+                <div className="flex justify-between items-center mb-4">
+                  <input
+                    type="text"
+                    name="nuevoCurso"
+                    value={formData.nuevoCurso || ''}
+                    onChange={handleInputChange}
+                    className="p-2 border rounded-md w-3/4"
+                    placeholder="Nuevo Curso"
+                  />
+                  <input
+                    type="text"
+                    name="nuevoNivel"
+                    value={formData.nuevoNivel || ''}
+                    onChange={handleInputChange}
+                    className="p-2 border rounded-md w-1/4"
+                    placeholder="Nivel"
+                  />
+                  <button
+                    type="button"
+                    onClick={handleAddCourse}
+                    className="text-green-500"
+                  >
+                    Agregar
+                  </button>
+                </div>
+
+                <button type="submit" className="mt-4 bg-blue-500 text-white p-2 rounded-md w-full">
+                  Actualizar
+                </button>
+              </div>
+            </form>
             <button
-              type="button"
-              onClick={handleAddCourse}
-              className="text-green-500"
+              onClick={handleModalToggle}
+              className="mt-4 bg-gray-500 text-white p-2 rounded-md w-full"
             >
-              Agregar
+              Cerrar
             </button>
           </div>
-
-          <button type="submit" className="mt-4 bg-blue-500 text-white p-2 rounded-md w-full">
-            Actualizar
-          </button>
         </div>
-      </form>
-      <button
-        onClick={handleModalToggle}
-        className="mt-4 bg-gray-500 text-white p-2 rounded-md w-full"
-      >
-        Cerrar
-      </button>
-    </div>
-  </div>
-)}
-
-
+      )}
     </div>
   );
 }
