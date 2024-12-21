a7a769cbf4411d07ea53842d6e0f47554401afc8
lerghin <lerghincardenas@gmail.com>
2024-12-21 18:48:29 -0400

    au

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -23,7 +23,7 @@ const RegisterPage = () => {
 
     setIsLoading(true);
     try {
-      const response = await axios.post(`${API_URL}admin/register`, {
+      const response = await axios.post(`${API_URL}auth/admin/register`, {
         userName,
         password,
       });
